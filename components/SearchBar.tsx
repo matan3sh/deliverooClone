e071c9b6455e6cc92ff0e811933111102e96f0b7
Matan <matan3sh@gmail.com>
2024-02-14 19:18:14 +0200

    Finish Custom Header Component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,13 +2,24 @@ import Colors from "@/constants/Colors"
 import { Ionicons } from "@expo/vector-icons"
 import { Link } from "expo-router"
 import React from "react"
-import { StyleSheet, TouchableOpacity, View } from "react-native"
+import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native"
 
 const SearchBar = () => {
   return (
     <View style={styles.searchContainer}>
       <View style={styles.searchSection}>
-        <View style={styles.searchField}></View>
+        <View style={styles.searchField}>
+          <Ionicons
+            style={styles.searchIcon}
+            name="ios-search"
+            size={20}
+            color={Colors.medium}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Resturants, groceries, dishes"
+          />
+        </View>
         <Link href={"/"} asChild>
           <TouchableOpacity style={styles.optionButton}>
             <Ionicons name="options-outline" size={20} color={Colors.primary} />
@@ -24,9 +35,30 @@ const styles = StyleSheet.create({
     height: 60,
     backgroundColor: "#fff",
   },
-  searchSection: {},
-  searchField: {},
-  optionButton: {},
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    padding: 10,
+    color: Colors.mediumDark,
+  },
+  searchIcon: {
+    paddingLeft: 10,
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
+  },
 })
 
 export default SearchBar
